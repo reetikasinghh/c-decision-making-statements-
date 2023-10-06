d420fee781ac5d2aadd54def0e8694f4d33c567f
reetikasinghh <139485681+reetikasinghh@users.noreply.github.com>
2023-10-06 10:29:41 +0530

    Update switchcase.cpp

--- switchcase.cpp
+++ switchcase.cpp
@@ -50,3 +50,67 @@ int main()
 Saturday 
 Enter the Day's number :2     
 Tuesday*/
+
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+    float i, r; // Declare two floating-point variables 'i' and 'r' to store user input.
+    int calc = 4; // Initialize 'calc' with a default value of 4.
+
+    cout << "Enter the first Number :"; // Prompt the user to enter the first number.
+    cin >> i; // Read the first number from the user and store it in 'i'.
+
+    cout << "Enter the second Number :"; // Prompt the user to enter the second number.
+    cin >> r; // Read the second number from the user and store it in 'r'.
+
+    cout << "Enter the operation to execute :" << endl; // Prompt the user to choose an operation.
+    cout << "1. Sum" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cin >> calc; // Read the user's choice and store it in 'calc'.
+
+    switch (calc) // Start a switch statement based on the value of 'calc'.
+    {
+        case 1:
+            cout << "The sum is : " << i + r << endl; // If 'calc' is 1, calculate and print the sum.
+            break;
+
+        case 2:
+            cout << "The difference is : " << i - r << endl; // If 'calc' is 2, calculate and print the difference.
+            break;
+
+        case 3:
+            cout << "The product is : " << i * r << endl; // If 'calc' is 3, calculate and print the product.
+            break;
+
+        case 4:
+            if (r != 0) // Check if the second number is not zero to avoid division by zero.
+            {
+                cout << "The quotient is : " << i / r << endl; // If 'calc' is 4 and 'r' is not zero, calculate and print the quotient.
+            }
+            else
+            {
+                cout << "Division by zero is not allowed." << endl; // If 'calc' is 4 and 'r' is zero, show an error message.
+            }
+            break;
+
+        default:
+            cout << "Wrong Input"; // If 'calc' is not 1-4, print "Wrong Input".
+            break;
+    }
+
+    return 0; // Return 0 to indicate successful program execution.
+}
+
+/* output 
+1. Sum
+2. Subtraction
+3. Multiplication
+4. Division
+4
+The quotient is : 1.55556
+
+*/
