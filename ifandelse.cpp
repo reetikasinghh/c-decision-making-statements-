7fb9442a14d2950a154960f4605344b10eb92321
reetikasinghh <139485681+reetikasinghh@users.noreply.github.com>
2023-10-08 12:16:10 +0530

    Update ifandelse.cpp

--- ifandelse.cpp
+++ ifandelse.cpp
@@ -33,17 +33,39 @@ the number is odd*/
 
 
 
+//wap to print the largest number
+ #include <iostream>
+using namespace std;
 
- int x,y,z;
-    cout<<"Enter the first number:"<<endl;
-    cin>>x;
-    cout<<"Enter the Second number:"<<endl;
-    cin>>y;
-    cout<<"Enter the third number:"<<endl;
-    cin>>z;
-    if ((x>y) && (x>z))
-    cout<<"The first number is largest:"<<endl;
-    else if ((y>x) && (y>z))
-    cout<<"The second number is largest:"<<endl;
+int main() {
+    int x, y, z;
+
+    // Prompt the user to enter the first number
+    cout << "Enter the first number: ";
+    cin >> x;
+
+    // Prompt the user to enter the second number
+    cout << "Enter the second number: ";
+    cin >> y;
+
+    // Prompt the user to enter the third number
+    cout << "Enter the third number: ";
+    cin >> z;
+
+    // Check which number is the largest among the three
+    if (x > y && x > z)
+        cout << "The first number is largest." << endl;
+    else if (y > x && y > z)
+        cout << "The second number is largest." << endl;
+    else if (z > x && z > y)
+        cout << "The third number is largest." << endl;
     else
-    cout<<"The third number is largest:"<<endl;
+        cout << "Two or more numbers are equal and largest." << endl;
+
+    return 0;
+}
+/*Enter the first number: 10
+Enter the second number: 20
+Enter the third number: 15
+The second number is largest.
+*/
